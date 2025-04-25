7b0fde7b0f5099e260b8c225773e8d1f71dde46a
dubai-elektroniks-site <dubaielektroniks@gmail.com>
2025-04-25 21:08:10 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const App = () => {
+  return <h1>Welcome to Dubai Elektroniks</h1>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
